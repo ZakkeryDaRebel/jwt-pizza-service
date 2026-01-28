62bef2226eeb81c7c392b53ea9d1c6b40383af9a
Tristan Weech <twminebro@gmail.com>
2026-01-27 22:28:29 -0700

    Fix service.test.js

--- src/service.test.js
+++ src/service.test.js
@@ -3,6 +3,7 @@ const app = require('./service.js');
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken = null;
+let loginUserAuthToken = null;
 
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
@@ -10,20 +11,33 @@ beforeAll(async () => {
   testUserAuthToken = registerRes.body.token;
 });
 
-test('login', async () => {
-  const loginRes = await request(app).put('/api/auth').send(testUser);
-  expect(loginRes.status).toBe(200);
-  expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+describe('authRouter tests', () => {
+    test('login', async () => {
+        const loginRes = await request(app).put('/api/auth').send(testUser);
+        expect(loginRes.status).toBe(200);
+        expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+        loginUserAuthToken = loginRes.body.token;
 
-  const {...user } = { ...testUser, roles: [{ role: 'diner' }] };
-  delete user.password;
-  expect(loginRes.body.user).toMatchObject(user);
-  expect(loginRes.body.token).not.toBe(testUserAuthToken);
+        const {...user } = { ...testUser, roles: [{ role: 'diner' }] };
+        delete user.password;
+        expect(loginRes.body.user).toMatchObject(user);
+        expect(loginRes.body.token).not.toBe(testUserAuthToken);
+    });
+
+    test('logout', async () => {
+        const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${loginUserAuthToken}`);
+        expect(logoutRes.status).toBe(200);
+        expect(logoutRes.body.message).toBe('logout successful');
+    });
 });
 
-test('get cities', async () => {
-  const getCitiesRes = await request(app).get('/cities');
-  expect(getCitiesRes.status).toBe(200);
-  expect(getCitiesRes.headers['content-type']).toMatch('application/json; charset=utf-8');
-  expect(getCitiesRes.body).toMatchObject([{ name: 'Provo', population: 116618 }]);
+describe('franchiseRouter tests', () => {
+    test('list franchises', async () => {
+        const res = await request(app).get('/api/franchise?page=0&limit=10&name=*').send();
+        expect(res.status).toBe(200);
+        expect(res.body).toHaveProperty('franchises');
+        expect(Array.isArray(res.body.franchises)).toBe(true);
+        expect(res.body).toHaveProperty('more');
+        expect(typeof res.body.more).toBe('boolean');
+    });
 });
