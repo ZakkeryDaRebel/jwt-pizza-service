553056348c4276516df8ce6cafb3fcb43daa6234
Tristan Weech <twminebro@gmail.com>
2026-03-05 14:44:47 -0700

    Updating generatePizzaData.sh to also add 10 users

--- src/generatePizzaData.sh
+++ src/generatePizzaData.sh
@@ -1,4 +1,5 @@
 # Build this code by calling `chmod +x generatePizzaData.sh`
+# Run with ./generatePizzaData.sh <host>
 
 # Check if host is provided as a command line argument
 if [ -z "$1" ]; then
@@ -14,6 +15,17 @@ token=$(echo $response | jq -r '.token')
 # Add users
 curl -X POST $host/api/auth -d '{"name":"pizza diner", "email":"d@jwt.com", "password":"diner"}' -H 'Content-Type: application/json'
 curl -X POST $host/api/auth -d '{"name":"pizza franchisee", "email":"f@jwt.com", "password":"franchisee"}' -H 'Content-Type: application/json'
+
+curl -X POST $host/api/auth -d '{"name":"user1", "email":"u1@jwt.com", "password":"user1"}' -H 'Content-Type: application/json'
+curl -X POST $host/api/auth -d '{"name":"user2", "email":"u2@jwt.com", "password":"user2"}' -H 'Content-Type: application/json'
+curl -X POST $host/api/auth -d '{"name":"user3", "email":"u3@jwt.com", "password":"user3"}' -H 'Content-Type: application/json'
+curl -X POST $host/api/auth -d '{"name":"user4", "email":"u4@jwt.com", "password":"user4"}' -H 'Content-Type: application/json'
+curl -X POST $host/api/auth -d '{"name":"user5", "email":"u5@jwt.com", "password":"user5"}' -H 'Content-Type: application/json'
+curl -X POST $host/api/auth -d '{"name":"user6", "email":"u6@jwt.com", "password":"user6"}' -H 'Content-Type: application/json'
+curl -X POST $host/api/auth -d '{"name":"user7", "email":"u7@jwt.com", "password":"user7"}' -H 'Content-Type: application/json'
+curl -X POST $host/api/auth -d '{"name":"user8", "email":"u8@jwt.com", "password":"user8"}' -H 'Content-Type: application/json'
+curl -X POST $host/api/auth -d '{"name":"user9", "email":"u9@jwt.com", "password":"user9"}' -H 'Content-Type: application/json'
+curl -X POST $host/api/auth -d '{"name":"user10", "email":"u10@jwt.com", "password":"user10"}' -H 'Content-Type: application/json'
 
 # Add menu
 curl -X PUT $host/api/order/menu -H 'Content-Type: application/json' -d '{ "title":"Veggie", "description": "A garden of delight", "image":"pizza1.png", "price": 0.0038 }'  -H "Authorization: Bearer $token"
