f4e5e7bfdbf402f0ef6b93b54e1710edaefca0ce
Tristan Weech <twminebro@gmail.com>
2026-03-11 17:55:02 -0600

    Because the metrics aren't working, I'm commenting them out at the moment

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,11 +2,11 @@ const express = require('express');
 const { asyncHandler } = require('../endpointHelper.js');
 const { DB, Role } = require('../database/database.js');
 const { authRouter, setAuth } = require('./authRouter.js');
-const metrics = require('../metrics.js');
-const app = express();
+//const metrics = require('../metrics.js');
+//const app = express();
 
 const userRouter = express.Router();
-app.use(metrics.requestTracker);
+//app.use(metrics.requestTracker);
 
 userRouter.docs = [
   {
