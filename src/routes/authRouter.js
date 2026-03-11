084dc774ea009b4e8a136bb43134d0c4cb6f216d
Tristan Weech <twminebro@gmail.com>
2026-03-11 12:32:52 -0600

    Added authMetrics not only to login, but also to register

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -78,13 +78,20 @@ authRouter.authenticateToken = (req, res, next) => {
 authRouter.post(
   '/',
   asyncHandler(async (req, res) => {
-    const { name, email, password } = req.body;
-    if (!name || !email || !password) {
-      return res.status(400).json({ message: 'name, email, and password are required' });
+    try {
+      const { name, email, password } = req.body;
+      if (!name || !email || !password) {
+        metrics.authAttempt(false);
+        return res.status(400).json({ message: 'name, email, and password are required' });
+      }
+      const user = await DB.addUser({ name, email, password, roles: [{ role: Role.Diner }] });
+      const auth = await setAuth(user);
+      metrics.authAttempt(true);
+      res.json({ user: user, token: auth });
+    } catch (e) {
+      metrics.authAttempt(false);
+      throw e;
     }
-    const user = await DB.addUser({ name, email, password, roles: [{ role: Role.Diner }] });
-    const auth = await setAuth(user);
-    res.json({ user: user, token: auth });
   })
 );
 
