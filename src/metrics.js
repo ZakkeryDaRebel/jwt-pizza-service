02176aca750f6e52e3a1d9c8b7b9cf9d4f79617d
Tristan Weech <twminebro@gmail.com>
2026-03-16 08:53:32 -0600

    Going back to using userId for active users metric

--- src/metrics.js
+++ src/metrics.js
@@ -21,8 +21,8 @@ function requestTracker(req, res, next) {
 
   //If the user has an id, and has sent a backend request,
   //  then it is considered an active user
-  if (req.user) {
-    recordUserActivity(req.user);
+  if (req.user && (req.user.id || req.user.userId)) {
+    recordUserActivity(req.user.id || req.user.userId);
   }
 
   next();
@@ -111,18 +111,18 @@ const activityQueue = [];
 const minutesInMS = 60 * 1000;
 const activeTime = 15 * minutesInMS; 
 
-function recordUserActivity(user) {
-    if (!user) return;
+function recordUserActivity(userId) {
+    if (!userId) return;
 
     const now = Date.now();
 
-    activeUsers.set(user, now);
-    activityQueue.push({ user, time: now });
+    activeUsers.set(userId, now);
+    activityQueue.push({ userId, time: now });
 }
 
-function userLoggedOut(user) {
-    if (!user) return;
-    activeUsers.delete(user);
+function userLoggedOut(userId) {
+    if (!userId) return;
+    activeUsers.delete(userId);
 }
 
 function cleanupInactiveUsers() {
